ea4f4b1897089b4766bf57d67ecfc0030419f310
Shawn Hernan <shawnhernan@hotmail.com>
2020-05-03 20:18:38 -0700

    Original version

---
+++ SamplingSimulator/Population.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace SamplingSimulator
+{
+
+    class Population
+    {
+        private Dictionary<uint, bool> thePopulation = new Dictionary<uint, bool>();
+        private Random rnd = new Random();
+        private double roll;
+        public uint trues {get;}
+        public  uint falses { get; }
+
+        public Population(uint size, double probability)
+        {
+            trues = 0;
+            falses = 0; 
+            for (uint i = 0; i < size; i++)
+            {
+                roll = rnd.NextDouble(); 
+                if (roll < probability)
+                {
+                    thePopulation.Add(i, true);
+                    trues++; 
+                }
+                else
+                {
+                    thePopulation.Add(i, false);
+                    falses++; 
+                }
+            }
+        }
+
+        public void Print()
+        {
+            foreach (KeyValuePair<uint, bool> kvp in thePopulation)
+            {
+                Console.WriteLine("Entry: {0}, value: {1}", kvp.Key, kvp.Value); 
+            }
+        }
+    }
+}
--- SamplingSimulator/Program.cs
+++ SamplingSimulator/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+// this is a tool to simulate sampling from a set of binary choices. 
+// 
+
 namespace SamplingSimulator
 {
     class Program
@@ -7,6 +10,9 @@ namespace SamplingSimulator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Population thePopulation = new Population(5000, 0.5);
+            //thePopulation.Print();
+            Console.WriteLine("total true: {0}, total false: {1}", thePopulation.trues, thePopulation.falses); 
         }
     }
 }
