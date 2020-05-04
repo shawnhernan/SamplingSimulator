8bf6b6fce6c84b8f5db91c933221b2ecd2d1d361
Shawn Hernan <shawnhernan@hotmail.com>
2020-05-03 19:36:57 -0700

    Add project files.

---
+++ SamplingSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SamplingSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
